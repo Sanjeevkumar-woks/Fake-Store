cd3d9c25d9afd4cf68be438869774da534cd79c9
Sanjeevkumar-woks <sanjeevmanagutti@gmail.com>
2024-09-11 17:30:37 +0530

    added title

--- src/Components/Loader.jsx
+++ src/Components/Loader.jsx
@@ -2,7 +2,7 @@ const Loader = () => {
   return (
     <div className="flex justify-center items-center h-screen w-screen">
       <div
-        class="w-12 h-12 rounded-full animate-spin
+        class="w-32 h-32 rounded-full animate-spin
                     border-2 border-solid border-blue-500 border-t-transparent  "
       ></div>
     </div>
